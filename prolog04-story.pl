25a9e3034190869f162508786f76b03463655aac
PedroNFarias <52089109+PedroNFarias@users.noreply.github.com>
2021-06-30 16:08:49 -0300

    Update prolog04-story.pl

--- prolog04-story.pl
+++ prolog04-story.pl
@@ -8,10 +8,10 @@ go :-
   Desenvolver = [Motivação, Habilidade, Plano, Final],
   
   Motivação = [
-    'depois', choose(['de sofrer bullying', 'de conhecer prolog', 'de ter visto sua família morrer para um dragão']), 'e'],
+    'depois', choose(['de sofrer bullying', 'de conhecer prolog', 'de ter visto sua família ser morta por um dragão', 'de ver sua amada ser morta por um amigo próximo']), 'e'],
     
   Habilidade = [
-    choose(['ser treinado por grandes magos', 'descobrir poderes que ele tinha que jamais tinha visto antes', 'ter uma ideia extremamente genial'])],
+    choose(['ser treinado por grandes magos', 'descobrir poderes que ele tinha que jamais tinha visto antes', 'ter uma ideia extremamente genial', 'descobrir um artefato poderoso'])],
   Plano = [
     choose(['e decidir que a raça humana é corrupta', ['jura vingança contra', Criatura]])],
   Criatura = [
@@ -19,9 +19,9 @@ go :-
   Final = [
     choose(['quer destruí-los', 'quer escravizá-los', 'quer trocar todos por ciborgues']), Método, Resultado],
   Método = [
-    'através', choose(['da magia', 'de invenções', 'da violência', 'da morte'])],
+    'através', choose(['da magia', 'de invenções', 'da violência', 'da morte', 'de um artefato poderoso'])],
   Resultado = [
-    'porém, no final', choose(['todo mundo morre', ['um grupo de heróis luta contra o vilão e o derrota', Herói]])],
+    'porém, no final', choose(['todo mundo morre', ['um grupo de heróis luta contra o vilão e o derrota', Herói], 'um item mágico que ele usava é destruído e ele é derrotado'])],
   Herói = [
      'consulte o programa do Júlio para criar seu herói'],
   tellStory(Story),!.
